006e4b786580854d6ff0b1115b809635762ac253
Kaarel Türkson <kaartur@greeny.cs.tlu.ee>
2016-02-03 10:42:57 +0200

    doesn't work

--- calc.js
+++ calc.js
@@ -1,1 +1,24 @@
-kala
+
+//ootame kuni leht on laetud
+
+
+window.onload =function(){
+
+  //saan kätte väärtused
+
+  var cm = document.getElementById('id');
+  var inch = document.getElementById('inch');
+
+  //lisan nuppudele kuularid
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+  document.getElementById('to_cm').addEventListener('click', function(){
+    cm.value = inch.value*2.54;
+  });
+};
+
+
+
+function convertToInch(){
+
+  inch.value = cm.value/2.54;
+}
